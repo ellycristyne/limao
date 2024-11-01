8a40db8b82db25e7bceedffb4c6a2ae255ee9ee6
elly.manuh <141268921+ellycristyne@users.noreply.github.com>
2024-11-01 05:15:04 -0300

    fdvx

--- perguntas.js
+++ perguntas.js
@@ -6,121 +6,121 @@ criaCartao(
 
 criaCartao(
     'História',
-    'Qual o nome do primeiro presidente do Brasil?',
-    'Marechal Manuel Deodoro da Fonseca'
+    'Em que ano começou a Segunda Guerra Mundial?',
+    'Iniciou em 1939.'
 )
 
 criaCartao(
     'Filosofia',
-    'De quem é a famosa frase “Penso, logo existo”?',
-    'Descartes'
+    'Quem é conhecido como o "pai da filosofia ocidental"?  ',
+    'Sócrates.'
 )
 
-
-criaCartao(
-    'História',
-    'Quais o menor e o maior país do mundo?',
-    'Vaticano e Rússia'
-)
-
-criaCartao(
-    'Português',
-    'Qual o sentido de um repertório em uma redação?',
-    'Colaborar com a discussão que está sendo construída'
-)
-
-criaCartao(
-    'Química',
-    'O que é um círculo aromático?',
-    'São compostos orgânicos cíclicos e planares que possuem ligações duplas alternadas'
-)
-
-criaCartao(
-    'Artes',
-    'Quem pintou "A Noite Estrelada"?',
-    'Vincent Van Gogh'
-)
 
 criaCartao(
     'Biologia',
-    'Qual é o tipo sanguíneo que pode ser doador universal?',
-    'O negativo'
-)
-
-criaCartao(
-    'Educação Financeira',
-    'O que é bolsa de valores?',
-    'É o lugar onde investidores se reúnem para comprar e vender alguns tipos de ativos, em busca de lucro e proteção de capital.'
-)
-
-criaCartao(
-    'Geografia',
-    'O que é uma bacia hidrográfica?',
-    'Toda a área de captação natural da água da chuva que escoa superficialmente para um corpo de água ou seu contribuinte'
-)
-
-criaCartao(
-    'Historía',
-    'Quando ocorreu a ditadura militar no Brasil?',
-    '1964'
-)
-
-criaCartao(
-    'Biologia',
-    'Quais destas doenças são sexualmente transmissíveis?',
-    'Gonorreia, clamídia e sífilis'
-)
-
-criaCartao(
-    'Matemática',
-    'Quantas casas decimais tem o número pi?',
-    'Infinitas'
-)
-
-criaCartao(
-    'Educação Física',
-    'Qual a altura da rede de vôlei nos jogos masculino e feminino?',
-    '2,43m e 2,24m'
-)
-
-criaCartao(
-    'Português',
-    'Quais os principais autores do estilo literário Barroco no Brasil?',
-    'Gregório de Matos, Bento Teixeira e Manuel Botelho de Oliveira'
-)
-
-criaCartao(
-    'História',
-    'Em que período da pré-história o fogo foi descoberto?',
-    'Paleolítico'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a montanha mais alta do Brasil?',
-    'Pico da Neblina'
+    'O que é Fotossíntese?',
+    'É o processo pelo qual as plantas convertem luz solar em energia química, utilizando dióxido de carbono e água.'
 )
 
 criaCartao(
     'Física',
-    'Qual a velocidade da luz?',
-    '299 792 458 metros por segundo (m/s)'
+    'O que é a lei da gravitação universal? ',
+    'É a lei que afirma que dois corpos se atraem com uma força que é proporcional ao produto de suas massas e inversamente proporcional ao quadrado da distância entre eles.'
+)
+
+criaCartao(
+    'Química',
+    'O que é um ácido?',
+    'É uma substância que libera íons de hidrogênio (H⁺) em solução aquosa.'
+)
+
+criaCartao(
+    'Artes',
+    'O que caracteriza o movimento impressionista na pintura?',
+    'A ênfase na luz, na cor e nas impressões momentâneas, ao invés de detalhes precisos.'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é a biodiversidade?',
+    'É a variedade de formas de vida em um determinado ambiente ou no planeta como um todo.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é inflação?',
+    'É o aumento generalizado dos preços de bens e serviços em uma economia.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 4x3?',
+    '12.'
+)
+
+criaCartao(
+    'Programação.',
+    'O que é a inteligência artificial? ',
+    'É a capacidade de máquinas e sistemas computacionais de realizar tarefas que normalmente requerem inteligência humana.'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é cultura? ',
+    'É o conjunto de valores, normas, costumes e práticas que caracterizam um grupo social.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que é a frequência cardíaca em repouso? ',
+    'É a quantidade de batimentos cardíacos por minuto quando a pessoa está em estado de repouso.'
 )
 
 criaCartao(
     'Inglês',
-    '“It is six twenty" ou "twenty past six”. Que horas são em inglês?',
-    '6:20'
+    'O que é "Good Morning" em português?',
+    'Bom Dia.'
+)
+
+criaCartao(
+    'Projeto de Vida',
+    'O que é resiliência? ',
+    'Resiliência é a capacidade de uma pessoa se recuperar de adversidades, enfrentar desafios e se adaptar a situações difíceis.'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'Qual é a diferença entre empreendedorismo social e empreendedorismo corporativo? ',
+    'O empreendedorismo social foca em resolver problemas sociais e ambientais, buscando impacto positivo, enquanto o empreendedorismo corporativo ocorre dentro de empresas, visando inovações que gerem lucro e competitividade.'
 )
 
 criaCartao(
     'Português',
-    'Quem é o autor de “O Príncipe”?',
-    'Maquiavel'
+    'O que é um adjetivo?',
+    'É uma palavra que descreve ou qualifica um substantivo.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é o planeta mais próximo do Sol? ',
+    ' Mercúrio.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o autor da teoria da evolução das espécies e em que obra ele a apresentou? ',
+    'Charles Darwin, na obra "A Origem das Espécies".'
 )
 
 criaCartao(
     'Geografia',
-    'Qual destes países é transcontinental?',
-    'Rússia'
+    ' Qual é o país com a maior área territorial do mundo? ',
+    'A Rússia.'
+)
+
+criaCartao(
+    'Química',
+    'O que é um elemento químico? ',
+    'É uma substância que não pode ser decomposta em substâncias mais simples e é composta por átomos que têm o mesmo número de prótons no núcleo.'
 )
