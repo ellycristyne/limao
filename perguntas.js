5c789e88b024adbb207f385e19f5bc1f2379bc84
elly.manuh <141268921+ellycristyne@users.noreply.github.com>
2024-11-01 04:00:11 -0300

    nhgh

---
+++ perguntas.js
@@ -0,0 +1,126 @@
+criaCartao(
+    'Biologia',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros.'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome do primeiro presidente do Brasil?',
+    'Marechal Manuel Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+)
+
+
+criaCartao(
+    'História',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
+)
+
+criaCartao(
+    'Português',
+    'Qual o sentido de um repertório em uma redação?',
+    'Colaborar com a discussão que está sendo construída'
+)
+
+criaCartao(
+    'Química',
+    'O que é um círculo aromático?',
+    'São compostos orgânicos cíclicos e planares que possuem ligações duplas alternadas'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou "A Noite Estrelada"?',
+    'Vincent Van Gogh'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o tipo sanguíneo que pode ser doador universal?',
+    'O negativo'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é bolsa de valores?',
+    'É o lugar onde investidores se reúnem para comprar e vender alguns tipos de ativos, em busca de lucro e proteção de capital.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é uma bacia hidrográfica?',
+    'Toda a área de captação natural da água da chuva que escoa superficialmente para um corpo de água ou seu contribuinte'
+)
+
+criaCartao(
+    'Historía',
+    'Quando ocorreu a ditadura militar no Brasil?',
+    '1964'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais destas doenças são sexualmente transmissíveis?',
+    'Gonorreia, clamídia e sífilis'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
+
+criaCartao(
+    'Educação Física',
+    'Qual a altura da rede de vôlei nos jogos masculino e feminino?',
+    '2,43m e 2,24m'
+)
+
+criaCartao(
+    'Português',
+    'Quais os principais autores do estilo literário Barroco no Brasil?',
+    'Gregório de Matos, Bento Teixeira e Manuel Botelho de Oliveira'
+)
+
+criaCartao(
+    'História',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Paleolítico'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do Brasil?',
+    'Pico da Neblina'
+)
+
+criaCartao(
+    'Física',
+    'Qual a velocidade da luz?',
+    '299 792 458 metros por segundo (m/s)'
+)
+
+criaCartao(
+    'Inglês',
+    '“It is six twenty" ou "twenty past six”. Que horas são em inglês?',
+    '6:20'
+)
+
+criaCartao(
+    'Português',
+    'Quem é o autor de “O Príncipe”?',
+    'Maquiavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual destes países é transcontinental?',
+    'Rússia'
+)
